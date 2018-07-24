090da4de5771a4637fd8f142a16a770278dd053c
Manuel Coenen <manuel.coenen@gmail.com>
2018-07-24 10:18:09 +0200

    Refactor JS code to reduce lines considerably and increase flow rate max to 300%

--- js/main.js
+++ js/main.js
@@ -3,186 +3,57 @@ $(document).ready(function() {
 	calcUStepsTable();
 });
 
-$('#line_width').change(function() {
-	calcExtrusionData();
-});
+$('#line_width').change(calcExtrusionData);
+$('#layer_height').change(calcExtrusionData);
+$('#flow').change(calcExtrusionData);
+$('#filament_diameter').change(calcExtrusionData);
+$('#segment_size').change(calcExtrusionData);
 
-$('#layer_height').change(function() {
-	calcExtrusionData();
-});
-
-$('#flow').change(function() {
-	calcExtrusionData();
-});
-
-$('#filament_diameter').change(function() {
-	calcExtrusionData();
-});
-
-$('#segment_size').change(function() {
-	calcExtrusionData();
-	calcUStepsTable();
-});
-
-$('#extrusion_on_segment').on('change', function() {
-	calcUStepsTable();
-});
-
-$('#extruder_steps').change(function() {
-	calcUStepsTable();
-});
-
-$('#retraction_speed').change(function() {
-	calcUStepsTable();
-});
-
-$('#step_rate').change(function() {
-	calcUStepsTable();
-});
-
-$('#retraction_speed').change(function() {
-	calcUStepsTable();
-});
+$('#extrusion_on_segment').on('change', calcUStepsTable);
+$('#extruder_steps').change(calcUStepsTable);
+$('#retraction_speed').change(calcUStepsTable);
+$('#step_rate').change(calcUStepsTable);
+$('#retraction_speed').change(calcUStepsTable);
 
 function calcUStepsTable() {
-	var extruderSteps16 = parseFloat($('#extruder_steps').val());
-	var stepRate = parseInt($('#step_rate').val()) * 1000;
-	var retractionSpeed = parseInt($('#retraction_speed').val());
-	var extrusionOnSegment = parseFloat($('#extrusion_on_segment').text());
-	var segmentSize = parseFloat($('#segment_size').val());
+	var extruderSteps16    = parseFloat($('#extruder_steps').val()),
+		stepRate           = parseInt($('#step_rate').val()) * 1000,
+		retractionSpeed    = parseInt($('#retraction_speed').val()),
+		extrusionOnSegment = parseFloat($('#extrusion_on_segment').text()),
+		segmentSize        = parseFloat($('#segment_size').val()),
 
-	var extruderSteps1   = extruderSteps16/16,
-		extruderSteps2   = extruderSteps16/8,
-		extruderSteps4   = extruderSteps16/4,
-		extruderSteps8   = extruderSteps16/2,
-		extruderSteps32  = extruderSteps16*2,
-		extruderSteps64  = extruderSteps16*4,
-		extruderSteps128 = extruderSteps16*8,
-		extruderSteps256 = extruderSteps16*16
+		steps              = [1, 2, 4, 8, 16, 32, 64, 128, 256],
+		extruderSteps      = [
+			extruderSteps16 / 16,
+			extruderSteps16 / 8,
+			extruderSteps16 / 4,
+			extruderSteps16 / 2,
+			extruderSteps16,
+			extruderSteps16 * 2,
+			extruderSteps16 * 4,
+			extruderSteps16 * 8,
+			extruderSteps16 * 16
+		];
 
-	    mmUstep1   = 1/extruderSteps1,
-		mmUstep2   = 1/extruderSteps2,
-		mmUstep4   = 1/extruderSteps4,
-		mmUstep8   = 1/extruderSteps8,
-		mmUstep16  = 1/extruderSteps16,
-		mmUstep32  = 1/extruderSteps32,
-		mmUstep64  = 1/extruderSteps64,
-		mmUstep128 = 1/extruderSteps128,
-		mmUstep256 = 1/extruderSteps256
+	for (var i = 0; i < steps.length; i++) {
+		var mmUstep         = (1 / extruderSteps[i]),
+			maxRetract      = (stepRate / extruderSteps[i]) * 60,
+			uStepsOnSegment = (extrusionOnSegment / mmUstep);
 
-	    maxRetract1   = (stepRate/extruderSteps1)*60,
-		maxRetract2   = (stepRate/extruderSteps2)*60,
-		maxRetract4   = (stepRate/extruderSteps4)*60,
-		maxRetract8   = (stepRate/extruderSteps8)*60,
-		maxRetract16  = (stepRate/extruderSteps16)*60,
-		maxRetract32  = (stepRate/extruderSteps32)*60,
-		maxRetract64  = (stepRate/extruderSteps64)*60,
-		maxRetract128 = (stepRate/extruderSteps128)*60,
-		maxRetract256 = (stepRate/extruderSteps256)*60
+		$('#steps_mm_' + steps[i]).text(extruderSteps[i].toFixed(3));
 
-	    uStepsOnSegment1   = (extrusionOnSegment/mmUstep1),
-		uStepsOnSegment2   = (extrusionOnSegment/mmUstep2),
-		uStepsOnSegment4   = (extrusionOnSegment/mmUstep4),
-		uStepsOnSegment8   = (extrusionOnSegment/mmUstep8),
-		uStepsOnSegment16  = (extrusionOnSegment/mmUstep16),
-		uStepsOnSegment32  = (extrusionOnSegment/mmUstep32),
-		uStepsOnSegment64  = (extrusionOnSegment/mmUstep64),
-		uStepsOnSegment128 = (extrusionOnSegment/mmUstep128),
-		uStepsOnSegment256 = (extrusionOnSegment/mmUstep256);
+		$('#mm_ustep_' + steps[i]).text(mmUstep.toFixed(5));
 
-	$('#steps_mm_1').text(extruderSteps1.toFixed(3));
-	$('#steps_mm_2').text(extruderSteps2.toFixed(3));
-	$('#steps_mm_4').text(extruderSteps4.toFixed(3));
-	$('#steps_mm_8').text(extruderSteps8.toFixed(3));
-	$('#steps_mm_16').text(extruderSteps16.toFixed(3));
-	$('#steps_mm_32').text(extruderSteps32.toFixed(3));
-	$('#steps_mm_64').text(extruderSteps64.toFixed(3));
-	$('#steps_mm_128').text(extruderSteps128.toFixed(3));
-	$('#steps_mm_256').text(extruderSteps256.toFixed(3));
+		$('#max_retract_' + steps[i])
+			.text(maxRetract.toFixed(1))
+			.addClass(maxRetract >= retractionSpeed ? 'text-success' : 'text-danger')
+			.removeClass(maxRetract < retractionSpeed ? 'text-success' : 'text-danger');
 
-	$('#mm_ustep_1').text(mmUstep1.toFixed(5));
-	$('#mm_ustep_2').text(mmUstep2.toFixed(5));
-	$('#mm_ustep_4').text(mmUstep4.toFixed(5));
-	$('#mm_ustep_8').text(mmUstep8.toFixed(5));
-	$('#mm_ustep_16').text(mmUstep16.toFixed(5));
-	$('#mm_ustep_32').text(mmUstep32.toFixed(5));
-	$('#mm_ustep_64').text(mmUstep64.toFixed(5));
-	$('#mm_ustep_128').text(mmUstep128.toFixed(5));
-	$('#mm_ustep_256').text(mmUstep256.toFixed(5));
-
-	$('#max_retract_1')
-		.text(maxRetract1.toFixed(1))
-		.addClass(maxRetract1 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract1 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_2')
-		.text(maxRetract2.toFixed(1))
-		.addClass(maxRetract2 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract2 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_4')
-		.text(maxRetract4.toFixed(1))
-		.addClass(maxRetract4 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract4 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_8')
-		.text(maxRetract8.toFixed(1))
-		.addClass(maxRetract8 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract8 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_16')
-		.text(maxRetract16.toFixed(1))
-		.addClass(maxRetract16 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract16 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_32')
-		.text(maxRetract32.toFixed(1))
-		.addClass(maxRetract32 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract32 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_64')
-		.text(maxRetract64.toFixed(1))
-		.addClass(maxRetract64 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract64 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_128')
-		.text(maxRetract128.toFixed(1))
-		.addClass(maxRetract128 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract128 < retractionSpeed ? 'text-success' : 'text-danger');
-	$('#max_retract_256')
-		.text(maxRetract256.toFixed(1))
-		.addClass(maxRetract256 >= retractionSpeed ? 'text-success' : 'text-danger')
-		.removeClass(maxRetract256 < retractionSpeed ? 'text-success' : 'text-danger');
-
-	$('#usteps_on_segment_1')
-		.text(uStepsOnSegment1.toFixed(5))
-		.addClass(uStepsOnSegment1 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment1 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_2')
-		.text(uStepsOnSegment2.toFixed(5))
-		.addClass(uStepsOnSegment2 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment2 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_4')
-		.text(uStepsOnSegment4.toFixed(5))
-		.addClass(uStepsOnSegment4 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment4 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_8')
-		.text(uStepsOnSegment8.toFixed(5))
-		.addClass(uStepsOnSegment8 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment8 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_16')
-		.text(uStepsOnSegment16.toFixed(5))
-		.addClass(uStepsOnSegment16 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment16 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_32')
-		.text(uStepsOnSegment32.toFixed(5))
-		.addClass(uStepsOnSegment32 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment32 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_64')
-		.text(uStepsOnSegment64.toFixed(5))
-		.addClass(uStepsOnSegment64 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment64 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_128')
-		.text(uStepsOnSegment128.toFixed(5))
-		.addClass(uStepsOnSegment128 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment128 < 1 ? 'text-success' : 'text-danger');
-	$('#usteps_on_segment_256')
-		.text(uStepsOnSegment256.toFixed(5))
-		.addClass(uStepsOnSegment256 >= 1 ? 'text-success' : 'text-danger')
-		.removeClass(uStepsOnSegment256 < 1 ? 'text-success' : 'text-danger');
+		$('#usteps_on_segment_' + steps[i])
+			.text(uStepsOnSegment.toFixed(5))
+			.addClass(uStepsOnSegment >= 1 ? 'text-success' : 'text-danger')
+			.removeClass(uStepsOnSegment < 1 ? 'text-success' : 'text-danger');
+	}
 }
 
 function calcExtrusionData() {
@@ -212,6 +83,8 @@ function calcExtrusionData() {
 	var segmentSize = parseFloat($('#segment_size').val());
 	var extrusionOnSegment = segmentSize * inputOutputRatio;
 
-	$('#extrusion_on_segment').text(extrusionOnSegment.toFixed(5)).trigger('change');
+	$('#extrusion_on_segment')
+		.text(extrusionOnSegment.toFixed(5))
+		.trigger('change');
 }
 
