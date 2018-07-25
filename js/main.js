55e976609444d52153c40001366a5d0ef66a1307
Manuel Coenen <manuel.coenen@gmail.com>
2018-07-25 12:07:39 +0200

    Refactor JS code into namespace and remove (already hidden) table of intermediate values

--- js/main.js
+++ js/main.js
@@ -1,82 +1,111 @@
-$(document).ready(function() {
-	calcExtrusionData();
-	calcUStepsTable();
-});
+(function(ustepCalculator, $, undefined) {
+	const	steps      = [   1,   2,   4,   8, 16, 32, 64, 128, 256],
+			multiplier = [1/16, 1/8, 1/4, 1/2,  1,  2,  4,   8,  16];
 
-$('#line_width').change(calcExtrusionData);
-$('#layer_height').change(calcExtrusionData);
-$('#flow').change(calcExtrusionData);
-$('#filament_diameter').change(calcExtrusionData);
-$('#segment_size').change(calcExtrusionData);
+	// Defaults
+	let lineWidth       = 0.4,
+		layerHeight     = 0.2,
+		flowRate        = 1,
+		segmentSize     = 0.2,
+		extruderSteps16 = 415,
+		stepRate        = 200 * 1000,
+		retractionSpeed = 1800,
+		filamentArea        = (1.75/2 * 1.75/2) * Math.PI,
+		extrusionInMM3PerMM = lineWidth * layerHeight * flowRate,
+		inputOutputRatio    = extrusionInMM3PerMM / filamentArea,
+		extrusionOnSegment  = segmentSize * inputOutputRatio;
 
-$('#extrusion_on_segment').on('change', calcUStepsTable);
-$('#extruder_steps').change(calcUStepsTable);
-$('#retraction_speed').change(calcUStepsTable);
-$('#step_rate').change(calcUStepsTable);
-$('#retraction_speed').change(calcUStepsTable);
+	let calcUStepsTable = function() {
+		for (let i = 0; i < steps.length; i++) {
+			let extruderSteps     = (extruderSteps16 * multiplier[i])
+				mmUstep           = (1 / extruderSteps),
+				maxRetract        = (stepRate / extruderSteps) * 60,
+				maxRetractOK      = maxRetract >= retractionSpeed,
+				uStepsOnSegment   = (extrusionOnSegment / mmUstep),
+				uStepsOnSegmentOK = uStepsOnSegment >= 1;
 
-function calcUStepsTable() {
-	var extruderSteps16    = parseFloat($('#extruder_steps').val()),
-		stepRate           = parseInt($('#step_rate').val()) * 1000,
-		retractionSpeed    = parseInt($('#retraction_speed').val()),
-		// TODO: This needs to go to a variable and not parsed from hidden contents
-		extrusionOnSegment = parseFloat($('#extrusion_on_segment').text()),
-		segmentSize        = parseFloat($('#segment_size').val()),
+			$('#steps_mm_' + steps[i]).text(extruderSteps.toFixed(3));
 
-		steps              = [1, 2, 4, 8, 16, 32, 64, 128, 256],
-		multiplier         = [1/16, 1/8, 1/4, 1/2, 1, 2, 4, 8 ,16];
+			$('#mm_ustep_' + steps[i]).text(mmUstep.toFixed(5));
 
-	for (var i = 0; i < steps.length; i++) {
-		var extruderSteps   = (extruderSteps16 * multiplier[i])
-			mmUstep         = (1 / extruderSteps),
-			maxRetract      = (stepRate / extruderSteps) * 60,
-			uStepsOnSegment = (extrusionOnSegment / mmUstep);
+			$('#max_retract_' + steps[i])
+				.text(maxRetract.toFixed(1))
+				.toggleClass('text-success', maxRetractOK)
+				.toggleClass('text-danger',  !maxRetractOK);
 
-		$('#steps_mm_' + steps[i]).text(extruderSteps.toFixed(3));
+			$('#usteps_on_segment_' + steps[i])
+				.text(uStepsOnSegment.toFixed(5))
+				.toggleClass('text-success', uStepsOnSegmentOK)
+				.toggleClass('text-danger',  !uStepsOnSegmentOK);
+		}
+	};
 
-		$('#mm_ustep_' + steps[i]).text(mmUstep.toFixed(5));
+	let calcExtrusionInMM3PerMM = function() {
+		extrusionInMM3PerMM = lineWidth * layerHeight * flowRate;
 
-		$('#max_retract_' + steps[i])
-			.text(maxRetract.toFixed(1))
-			.addClass(maxRetract >= retractionSpeed ? 'text-success' : 'text-danger')
-			.removeClass(maxRetract < retractionSpeed ? 'text-success' : 'text-danger');
+		calcInputOutputRatio();
+	};
 
-		$('#usteps_on_segment_' + steps[i])
-			.text(uStepsOnSegment.toFixed(5))
-			.addClass(uStepsOnSegment >= 1 ? 'text-success' : 'text-danger')
-			.removeClass(uStepsOnSegment < 1 ? 'text-success' : 'text-danger');
+	let calcFilamentArea = function(filamentRadius) {
+		filamentArea = (filamentRadius * filamentRadius) * Math.PI;
+
+		calcInputOutputRatio();
+	};
+
+	let calcInputOutputRatio = function() {
+		inputOutputRatio = extrusionInMM3PerMM / filamentArea;
+
+		calcExtrusionOnSegment();
+	};
+
+	let calcExtrusionOnSegment = function() {
+		extrusionOnSegment = segmentSize * inputOutputRatio;
+
+		calcUStepsTable();
+	};
+
+	let registerChangeHandlers = function() {
+		$('#line_width').change(function() {
+			lineWidth = parseFloat(this.value);
+			calcExtrusionInMM3PerMM();
+		});
+		$('#layer_height').change(function() {
+			layerHeight = parseFloat(this.value);
+			calcExtrusionInMM3PerMM();
+		});
+		$('#flow').change(function() {
+			flowRate = parseFloat(this.value) / 100.0;
+			calcExtrusionInMM3PerMM();
+		});
+		$('#filament_diameter').change(function() {
+			let filamentRadius = parseFloat(this.value) / 2.0;
+			calcFilamentArea(filamentRadius);
+		});
+		$('#segment_size').change(function() {
+			segmentSize = parseFloat(this.value);
+			calcExtrusionOnSegment();
+		});
+
+		$('#extruder_steps').change(function() {
+			extruderSteps16 = parseFloat(this.value);
+			calcUStepsTable();
+		});
+		$('#retraction_speed').change(function() {
+			retractionSpeed = parseInt(this.value);
+			calcUStepsTable();
+		});
+		$('#step_rate').change(function() {
+			stepRate = parseInt(this.value) * 1000;
+			calcUStepsTable();
+		});
+	};
+
+	ustepCalculator.init = function() {
+		registerChangeHandlers();
+		calcUStepsTable();
 	}
-}
+}(window.ustepCalculator = window.ustepCalculator || {}, jQuery));
 
-function calcExtrusionData() {
-
-	// Extrusion in mm³/mm
-	var lineWidth = parseFloat($('#line_width').val());
-	var layerHeight = parseFloat($('#layer_height').val());
-	var flowRate = parseFloat($('#flow').val()) / 100.0;
-
-	var extrusionInMM3PerMM = lineWidth * layerHeight * flowRate;
-
-	$('#extrusion_in_mm3_mm').text(extrusionInMM3PerMM.toFixed(5));
-
-	// Filament cross-sectional area
-	var filamentDiameter = parseFloat($('#filament_diameter').val());
-	var filamentRadius = filamentDiameter / 2;
-	var filamentArea = (filamentRadius * filamentRadius) * Math.PI;
-
-	$('#filament_area').text(filamentArea.toFixed(5));
-
-	// Input-Output-Ratio
-	var inputOutputRatio = extrusionInMM3PerMM / filamentArea;
-
-	$('#input_output_ratio').text(inputOutputRatio.toFixed(5));
-
-	// Extrusion on segement
-	var segmentSize = parseFloat($('#segment_size').val());
-	var extrusionOnSegment = segmentSize * inputOutputRatio;
-
-	$('#extrusion_on_segment')
-		.text(extrusionOnSegment.toFixed(5))
-		.trigger('change');
-}
-
+$(document).ready(function() {
+	ustepCalculator.init();
+});
